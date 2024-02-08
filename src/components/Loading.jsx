cf8daa57f4c1f33040de4fc1665afbaa40ea9e1f
Hemazyn <hemazyn@gmail.com>
2024-02-08 15:51:31 +0100

    loading changed

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,21 +1,18 @@
-import { LineWave } from 'react-loader-spinner';
+import { Vortex } from 'react-loader-spinner';
 
 const Loading = () => {
      return (
           <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-               <LineWave
+               <Vortex
                     visible={true}
-                    height="100"
-                    width="100"
-                    color="#4fa94d"
-                    ariaLabel="line-wave-loading"
+                    height="80"
+                    width="80"
+                    ariaLabel="vortex-loading"
                     wrapperStyle={{}}
-                    wrapperClass=""
-                    firstLineColor="#272fcd"
-                    middleLineColor="#4fa94d"
-                    lastLineColor="#c5cbgd"
+                    wrapperClass="vortex-wrapper"
+                    colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
                />
-          </div>
+          </div >
      );
 };
 
